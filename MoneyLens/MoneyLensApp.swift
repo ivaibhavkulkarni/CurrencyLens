a067d3d2ba350e86064e5eced8bec46628f92ff3
Vaibhav Kulkarni <120665380+ivaibhavkulkarni@users.noreply.github.com>
2025-02-25 11:54:45 +0530

    Initial Commit

---
+++ MoneyLens/MoneyLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyLensApp.swift
+//  MoneyLens
+//
+//  Created by Vaibhav kulkarni on 25/02/25.
+//
+
+import SwiftUI
+
+@main
+struct MoneyLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
