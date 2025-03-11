96a4e84c3f730d66b921f208c4aaab2e5a843cc1
Vaibhav Kulkarni <120665380+ivaibhavkulkarni@users.noreply.github.com>
2025-03-11 20:17:31 +0530

    initial commit

--- MoneyLens/ContentView.swift
+++ MoneyLens/ContentView.swift
@@ -8,17 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isConvertMode = false
+    @State var showAppInfo = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            VStack {
+                Text(isConvertMode ? "Convert the Currency" : "Scan the Currency")
+                    .font(.title)
+                    .fontWeight(.bold)
+                
+                Image(systemName: isConvertMode ? "repeat" : "scanner")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .foregroundColor(.black)
+                
+                Spacer()
+                
+                // Main Container
+                if isConvertMode {
+                    Text("Converter Container")
+                } else {
+                    Text("Scanner Container")
+                }
+                
+                Spacer()
+                
+                // Container for Horizontal images (Buttons)
+                HStack {
+                    Image(systemName: "scanner")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50)
+                        .foregroundColor(.black)
+                        .contentShape(Rectangle())
+                        .onTapGesture { isConvertMode = false }
+                    
+                    Image(systemName: "arrow.right.and.line.vertical.and.arrow.left")
+                        .mask(Rectangle().frame(width: 5, height: 50))
+                    
+                    Image(systemName: "repeat")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50)
+                        .foregroundColor(.black)
+                        .contentShape(Rectangle())
+                        .onTapGesture { isConvertMode.toggle() }
+                }
+                
+                HStack {
+                    Spacer()
+                    Button {
+                        showAppInfo.toggle()
+                    } label: {
+                        Image(systemName: "info.circle.fill")
+                            .font(.largeTitle)
+                            .foregroundStyle(.gray)
+                    }
+                    .padding(.trailing)
+                }
+                .sheet(isPresented: $showAppInfo) { AppInfo() }
+            }
         }
-        .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
